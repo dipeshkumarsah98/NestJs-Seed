98eaca8dd02b3ab9adddea5f63362232c4422c55
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2023-09-08 21:39:35 +0545

    seed create for user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,27 +5,48 @@ const prisma = new PrismaClient();
 
 async function main() {
   // create two dummy article
+
+  const user1 = await prisma.user.upsert({
+    where: { email: 'dipeshsah98@gmail.com' },
+    update: {},
+    create: {
+      email: 'dipeshsah98@gmail.com',
+      name: 'Dipesh sah',
+      password: 'dipeshsah123',
+    },
+  });
+  const user2 = await prisma.user.upsert({
+    where: { email: 'rupesh@gmail.com' },
+    update: {},
+    create: {
+      email: 'rupesh98@gmail.com',
+      name: 'Rupesh yadav',
+      password: 'rupeshyadav123',
+    },
+  });
   const post1 = await prisma.article.upsert({
     where: { title: 'Prisma support for mongoDB' },
-    update: {},
+    update: { authorId: user1.id },
     create: {
       title: 'Prisma Adds Support for MongoDB',
       body: 'Support for MongoDB has been one of the most requested features since the initial release of...',
       description:
         "We are excited to share that today's Prisma ORM release adds stable support for MongoDB!",
       published: false,
+      authorId: user1.id,
     },
   });
 
   const post2 = await prisma.article.upsert({
     where: { title: "What's new in Prisma? (Q1/22)" },
-    update: {},
+    update: { authorId: user2.id },
     create: {
       title: "What's new in Prisma? (Q1/22)",
       body: 'Our engineers have been working hard, issuing new releases with many improvements...',
       description:
         'Learn about everything in the Prisma ecosystem and community from January to March 2022.',
       published: true,
+      authorId: user2.id,
     },
   });
   console.log({ post1, post2 });
